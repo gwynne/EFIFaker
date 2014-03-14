21018b4a459c85d022768ed9028094816f487b47
Gwynne Raskind <gwynne@darkrainfall.org>
2014-03-13 23:52:58 -0500

    Correctly load stack parameters

--- EFIFaker/blocktramp_config.c
+++ EFIFaker/blocktramp_config.c
@@ -6,7 +6,7 @@
 		extern pl_trampoline_table_config pl_blockimp_table_page_config;
         pl_trampoline_table_config pl_blockimp_table_page_config = {
             .trampoline_size = 16,
-            .page_offset = 112,
-            .trampoline_count = 249,
+            .page_offset = 128,
+            .trampoline_count = 248,
             .template_page = &pl_blockimp_table_page
         };
